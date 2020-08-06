e46b8e4d8d2f891770c25839467e1ba684c6f993
seyoung <airseyoung@naver.com>
2020-08-06 13:24:04 +0900

    Add : posts delete()
    게시글 삭제 추가

--- src/main/java/com/springboot/webservice/service/PostsService.java
+++ src/main/java/com/springboot/webservice/service/PostsService.java
@@ -29,6 +29,7 @@ public class PostsService {
     // 트랜잭션이 끝나는 시점에 테이블에 변경을 반영한다. (Entity 객체 값만 변경하면 별도의 update 쿼리를 날릴 필요가 없다.)
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){
+        // 엔티티 조회 ( 존재 여부 확인 )
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
 
@@ -39,17 +40,29 @@ public class PostsService {
     // select (find)
     @Transactional
     public PostsResponseDto findById(Long id){
+        // 엔티티 조회 ( 존재 여부 확인 )
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
     }
 
+    // findAll
     @Transactional(readOnly=true)
     public List<PostsListResponseDto> findAllDesc(){
         // postsRepository 결과의 stream을 map을 통해 PostsListResponseDto로 변환해 List로 반환
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    // delete
+    @Transactional
+    public void delete(Long id){
+        // 엔티티 조회 ( 존재 여부 확인 )
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts); // JPA에서 지원하는 delete 메소드를 사용한다.
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,15 +1,20 @@
-// 필요한 모든 function을 정의하는 index 객체
-var index = {
-    init: function () {
+// 필요한 모든 function을 정의하는 main 객체
+var main = {
+    init : function () {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
         });
+
         $('#btn-update').on('click', function () {
             _this.update();
-        })
+        });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
-    save: function () {
+    save : function () {
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
@@ -19,17 +24,17 @@ var index = {
         $.ajax({
             type: 'POST',
             url: '/api/v1/posts',
-            dataTyep: 'json',
-            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function () {
+        }).done(function() {
             alert('글이 등록되었습니다.');
-            window.location.href = '/'
+            window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     },
-    update: function () {
+    update : function () {
         var data = {
             title: $('#title').val(),
             content: $('#content').val()
@@ -39,16 +44,31 @@ var index = {
 
         $.ajax({
             type: 'PUT',
-            url: '/api/v1/posts/' + id,
+            url: '/api/v1/posts/'+id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function () {
+        }).done(function() {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     }
 };
-index.init();
+main.init();
