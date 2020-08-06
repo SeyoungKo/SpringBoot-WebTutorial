2d7a755b42bb114636265fcd8905d68757558293
seyoung <airseyoung@naver.com>
2020-08-06 12:13:58 +0900

    Add : findAll()
    게시글 전체 조회하는 코드 추가

--- src/main/java/com/springboot/webservice/domain/posts/PostsRepository.java
+++ src/main/java/com/springboot/webservice/domain/posts/PostsRepository.java
@@ -1,8 +1,13 @@
 package com.springboot.webservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // Entity 클래스와 Entity Repository는 같은 위치에 있어야 한다. (함께 관리)
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
+    // findAll (SpringDataJPA에서 제공하지 않는 메소드는 쿼리로 작성해도 된다.)
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/springboot/webservice/web/IndexController.java
+++ src/main/java/com/springboot/webservice/web/IndexController.java
@@ -1,13 +1,25 @@
 package com.springboot.webservice.web;
 
+import com.springboot.webservice.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor // 롬복 생성자로 주입받기(의존성 관계가 변경될 때마다 생성자 코드를 번거롭게 수정하지 않아도 된다.)
 @Controller // 클라이언트 요청에 뷰를 반환하는 역할
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){ // PostService.findAllDesc() 결과를 index.mustache에 전달
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index"; //src/main/resources/templates/index.mustache 반환
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
